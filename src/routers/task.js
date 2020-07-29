3591cc49a495135cfceb55d440717815e0d468d6
aksharadyuti <47235698+aksharadyuti@users.noreply.github.com>
2020-07-30 01:56:49 +0530

    add pagination

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,6 +17,9 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+
+// GET/tasks?completed=true
+// GET/tasks?limit=10&skip=20
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
   
@@ -29,7 +32,11 @@ router.get('/tasks', auth, async (req, res) => {
         //const tasks = await Task.find({owner:req.user._id})
         await req.user.populate({
             path : 'tasks',
-            match 
+            match,
+            options:{
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
         }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
